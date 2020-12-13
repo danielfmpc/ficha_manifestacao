16ef3ad924839cef9310b5b1bde959df246430cd
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-12-13 17:49:05 -0300

    adicionando rotas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,14 @@
 import Router from 'express';
 import contatosRouter from './contatos.routes';
+import orgaosRouter from './orgaos.routes';
+import tiposRouter from './tipos.routes';
+import unidadesRouter from './unidades.routes';
 
 const routes = Router();
 
 routes.use('/contatos', contatosRouter);
+routes.use('/tipos', tiposRouter);
+routes.use('/orgaos', orgaosRouter);
+routes.use('/unidades', unidadesRouter);
 
 export default routes;
