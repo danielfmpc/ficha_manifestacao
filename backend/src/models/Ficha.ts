ae924f4cb7377ef1643f35a5929c2a3485af0bcd
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-12-18 00:17:46 -0300

    ajustando a model ficha

--- backend/src/models/Ficha.ts
+++ backend/src/models/Ficha.ts
@@ -12,16 +12,43 @@ class Ficha {
   id: string;
 
   @Column()
-  comentario: string;
+  nome: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  telefone: string;
+
+  @Column()
+  celular: string;
 
   @Column()
   sigilo: boolean;
+
+  @Column()
+  comentario: string;
+
+  @Column()
+  cep: string;
+
+  @Column()
+  numero: string;
+
+  @Column()
+  complemento: string;
 
   @Column()
   unidade_id: string;
 
   @Column()
   orgao_id: string;
+
+  @Column()
+  tipo_id: string;
+
+  @Column()
+  midia_id: string;
 
   @CreateDateColumn()
   created_at: Date;
