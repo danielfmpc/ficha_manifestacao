92d1d13c59d5663ad0c12dc21d6cfbf23070b234
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-12-18 00:18:09 -0300

    ajustando rota ficha

--- backend/src/routes/fichas.routes.ts
+++ backend/src/routes/fichas.routes.ts
@@ -1,6 +1,7 @@
 import Router from 'express';
 import { getRepository } from 'typeorm';
 import Ficha from '../models/Ficha';
+import ServiceCreateFichas from '../services/ServiceCreateFichas';
 
 const fichasRouter = Router();
 
@@ -8,6 +9,46 @@ fichasRouter.get('/', async (request, response) => {
   const fichaRepository = getRepository(Ficha);
   const ficha = await fichaRepository.find();
   return response.json(ficha);
+});
+
+fichasRouter.post('/', async (request, response) => {
+  try {
+    const {
+      celular,
+      cep,
+      complemento,
+      nome,
+      numero,
+      telefone,
+      email,
+      comentario,
+      sigilo,
+      midia_id,
+      orgao_id,
+      tipo_id,
+      unidade_id,
+    } = request.body;
+    const createServiceFichas = new ServiceCreateFichas();
+    const createEndereco = await createServiceFichas.execute({
+      celular,
+      cep,
+      complemento,
+      nome,
+      numero,
+      telefone,
+      email,
+      comentario,
+      sigilo,
+      unidade_id,
+      midia_id,
+      orgao_id,
+      tipo_id,
+    });
+
+    return response.json(createEndereco);
+  } catch (err) {
+    return response.status(401).json(err.message);
+  }
 });
 
 export default fichasRouter;
