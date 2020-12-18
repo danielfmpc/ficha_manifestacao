12970724fdad52b52c1358a75a74ffaac81fd6d6
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-12-18 00:24:27 -0300

    adicionando a rota midia

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import Router from 'express';
 import fichasRouter from './fichas.routes';
+import midiasRouter from './midias.routes';
 import orgaosRouter from './orgaos.routes';
 import tiposRouter from './tipos.routes';
 import unidadesRouter from './unidades.routes';
@@ -10,5 +11,6 @@ routes.use('/tipos', tiposRouter);
 routes.use('/orgaos', orgaosRouter);
 routes.use('/unidades', unidadesRouter);
 routes.use('/fichas', fichasRouter);
+routes.use('/midias', midiasRouter);
 
 export default routes;
